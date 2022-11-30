839926e94a0f908ae764188f8bd4dc529979db6e
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-11-30 20:48:20 -0300

    operaderes

---
+++ aula02/aula02.js
@@ -0,0 +1,36 @@
+document.write("Olá mundo")
+console.log("Hello, World!")
+
+let msg = "Minha mensagem" // texto
+let msg2 = 'Outra mensagem' // texto
+let meuPeso = 99.4 // número real
+let minhaAltura = 1.90 // número real
+let minhaIdade = 40 // númerp inteiro
+let ehDoador = false // valor booleano
+let teste = null // nulo
+
+// CRIAÇÃO DE CONSTANTES E ATRIBUIÇÃO DE VALORES
+const PI = 3.1415 
+const TAXA = 0.5
+
+teste = "Algum conteúdo"
+
+// ESCREVER O CONTEÚDO DAS VARIÁVEIS E CONSTANTES
+
+document.write("<p>Mensagem: " + msg + "</p>")
+
+document.write(`<p>Mensagem2: ${msg2}`) // outra forma de mostrar varáveis
+
+document.write("<p>Peso: " + meuPeso + " Kg</p>")
+
+document.write(`<p>Altura: ${minhaAltura} m`)
+
+document.write("<p>Idade: " + minhaIdade + "</p>")
+
+document.write(`<p>Doador? ${ehDoador}`)
+
+document.write("<p>Teste: " + teste + "</p>")
+
+document.write("<p>PI: " + PI + "</p>")
+
+document.write("<p>Taxa: " +TAXA + "</p>")
---
+++ aula03/aula03.js
@@ -0,0 +1,30 @@
+let n1 = 10 // número inteiro
+let n2 = 2 // número inteiro
+let n3 = "2" // string texto
+
+document.write(`N1 ${n1} <br>`)
+document.write(`N2 ${n2} <br>`)
+document.write(`N3 ${n3} <br>`)
+
+console.log(n1 + n2) // adição
+console.log(n1 - n2) // subtração
+console.log(n1 * n2) // multiplicação
+console.log(n1 / n2) // divisão
+console.log(n1 % n2) // resto ou módulo
+
+console.log(n2 == n3) // igual a
+console.log(n2 === n3) // identico a
+console.log(n2 != n3) // não é igual
+console.log(n2 !== n3) // não é identico
+
+console.log(n1 > n2) // maior que
+console.log(n1 < n2) // menor que
+console.log(n1 >= n2) // maior ou igual
+console.log(n1 <= n2) // menor ou igual
+
+let a = true
+let b = false
+console.log(a && b) // E / AND
+console.log(a || b) // OU / OR
+console.log(!a) // diferente de a / negação NOT
+console.log(!b) // diferente de b / negação NOT
