ecd079e932d09d200fe1e73fbad44e6a2314a4cb
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-01 18:31:51 -0300

    arrays

---
+++ aula04/aula04.js
@@ -0,0 +1,19 @@
+let produtos = ['Arroz','Feijão','Leite']
+var codigos = Array(10, 20, 30)
+let meses = ['Jan','Fev','Mar','Abr']
+
+//ADICIONAR "push" no final = empurre
+produtos.push('Açucar')
+codigos.push(40, 50, 60, 70)
+meses.push('Mai','Jun','Ago','07')
+
+// REMOVER do final pop = estourar
+produtos.pop()
+codigos.pop()
+meses.pop()
+
+// ADICIONAR no inicio = unshift
+produtos.unshift('Uva','Maçã')
+
+// REMOVER do inicio = shift
+produtos.shift()
