39334737110fd1c9c07f0ea684b88cd39e875be9
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-03 11:49:03 -0300

    desafio pt1

---
+++ aula06/aula6d.js
@@ -0,0 +1,27 @@
+let lutador = "João Gabriel"
+let nacionalidade = "Brasileiro"
+let idade = 19
+let peso = 99.7
+let altura = 1.90
+
+document.write(`Lutador: ${lutador}` + "<br>")
+document.write(`Nacionalidade: ${nacionalidade}` + "<br>")
+document.write(`Idade: ${idade}` + ' anos' + "<br>")
+document.write(`Peso: ${peso}` +  ' Kg' + "<br>")
+document.write(`Altura: ${altura}` + ' m' + "<br>" + "<hr>")
+
+document.write("Frase do dia" + "<br>");
+
+const dia = 03
+const mes = 12
+const ano = 2022
+const frase = "i know how it feels to be someone who loses their way"
+const autor = "Justin Bieber"
+
+document.write(dia + '/' + mes + '/' + ano + '<br>')
+document.write(frase + '<br>')
+document.write(autor + '<br>' + '<hr>')
+
+let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+document.write("Array de meses e ano<br>")
+document.write(meses[0] + ", " + meses[1] +", " + meses[2] +  ", " + meses[3] + ", " + meses[4] + ", " + meses[5] + ", " + meses[6] + ", " + meses[7] + ", " + meses[8] + ", " + meses[9] + ", " + meses[10] + ", " + meses[11])
