3ce433b2da07c1dfdcfef65b9296d971c9d1ded0
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-08 18:30:12 -0300

    pt 2 ex

---
+++ aula14/aula14.js
@@ -0,0 +1,12 @@
+// Aula 14 - manipular dados de formulário
+
+// SELECIONE TODOS OS ELEMENTOS QUE DESEJA MANIPULAR
+let formulario = document.querySelector('form')
+let cxNome = document.querySelector('#nome')
+let cxIdade = document.querySelector('#idade')
+let cxPeso = document.querySelector('#peso')
+let cxAltura = document.querySelector('#altura')
+let cxImc = document.querySelector('#imc')
+
+let aviso = document.querySelector('#aviso')
+let dados = document.querySelector('.pessoa')
