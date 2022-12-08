73740d535286d2706dab7966105fc39d1f0db6fb
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-08 20:42:12 -0300

    exercício de cálculo de imc

--- aula14/aula14.js
+++ aula14/aula14.js
@@ -6,7 +6,79 @@ let cxNome = document.querySelector('#nome')
 let cxIdade = document.querySelector('#idade')
 let cxPeso = document.querySelector('#peso')
 let cxAltura = document.querySelector('#altura')
-let cxImc = document.querySelector('#imc')
+let cxImc = document.querySelector('#resultadoImc')
 
 let aviso = document.querySelector('#aviso')
-let dados = document.querySelector('.pessoa')
+let dados = document.querySelectorAll('.pessoa')
+
+let btnEnviar = document.querySelector('#btnEnviar')
+let btnLimpar = document.querySelector('#btnLimpar')
+
+// PARA PEGAR OS DADOS QUE ESTÃO DENTRO DAS CAIXAS UTILIZE A PROPEIEDADE .value MAS, ANTES DETERMINE UM EVENTO COMO REFERÊNCIA PARA PEGAR OS DADOS
+
+// adicionamos um escutador para o btnEnviar 
+// uma função anônima para pegar os valores
+// calcular o imc
+btnEnviar.addEventListener('click', function(e){
+    // pegar os valores de cada input
+    let nome = cxNome.value
+    let idade = cxIdade.value
+    let peso = cxPeso.value
+    let altura = cxAltura.value
+    let imc = (peso / (altura * altura)).toFixed(1)
+
+    console.log(nome)
+    console.log(idade)
+    console.log(peso)
+    console.log(altura)
+    console.log(imc)
+
+    cxImc.value = imc
+    let sit = situacaoDoPeso(imc)
+    aviso.textContent = sit
+
+    // SAÍDA DE DADOS
+    // alert(
+    //     'Nome ' + nome + '\n' + 'Idade ' + idade + ' anos\n' + 'Peso ' + peso + 'Kg\n' + 'Altura ' + altura + 'm\n' + 'IMC ' + imc + ' ' + sit
+    // )
+
+    // criar objeto pessoa
+    let pessoa = {
+        nome : nome, // propriedade : variável
+        idade : idade,
+        peso : peso,
+        altura : altura,
+        imc : imc,
+        sit : sit
+    }
+
+    console.log(pessoa)
+    dados[1].textContent = pessoa.nome
+    dados[2].textContent = pessoa.idade + " anos"
+    dados[3].textContent = pessoa.peso + " Kg"
+    dados[4].textContent = pessoa.altura + " m"
+    dados[5].textContent = pessoa.imc + " " + pessoa.sit
+
+    // previnir comportamento padrão
+    e.preventDefault()
+})
+
+function situacaoDoPeso(imc) {
+    let situacao = ''
+    if (imc < 18.5) {
+        situacao = 'Peso baixo'
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        situacao = 'Peso normal'
+    } else if (imc >= 25 && imc < 29.9) {
+        situacao = 'Sobrepeso'
+    } else if (imc >= 30 && imc <= 34.9) {
+        situacao = 'Obesidade I'
+    } else if (imc >= 35 && imc < 39.9) {
+        situacao = 'Obesidade II'
+    } else if (imc > 40) {
+        situacao = 'Obesidade III'
+    } else {
+        situacao = 'Informe seu peso corretamente'
+    }
+    return situacao
+}
