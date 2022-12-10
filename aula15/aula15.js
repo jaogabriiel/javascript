824f741fef7d0484e138261498f7f004cddea796
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-10 09:24:33 -0300

    calculadora de média

--- aula15/aula15.js
+++ aula15/aula15.js
@@ -35,7 +35,75 @@ function situacaoFinal (mediaFinal) {
 function formatarSituacao (situacaoFinal) {
     console.log('Situação Final ' + situacaoFinal)
     switch(situacaoFinal) {
-        case 'Aprovado':
+
+        case 'Aprovado(a)':
             cxSituacao.classList.remove('reprovado')
+            cxSituacao.classList.remove('recuperação')
+            cxSituacao.classList.add('aprovado')
+            console.log('adicionar classe aprovado')
+            break
+
+        case 'Reprovado(a)':
+            cxSituacao.classList.remove('aprovado')
+            cxSituacao.classList.remove('recuperação')
+            cxSituacao.classList.add('reprovado')
+            console.log('adicionar classe reprovado')
+            break
+
+        case 'Recuperação':
+            cxSituacao.classList.remove('aprovado')
+            cxSituacao.classList.remove('reprovado')
+            cxSituacao.classList.add('recuperacao')
+            console.log('adicionar classe recuperação')
+            break
+
+        default:
+            console.log('Situação indefinda')
     }
 }
+
+// validar e gerar flash message
+function validarNumero(numero) {
+    let num1 = cxNota1.value
+    let num2 = cxNota2.value
+    if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10) {
+        formulario.reset() // limpar form
+        aviso.textContent = 'Digite uma nota entre 0.0 e 10.0'
+        aviso.classList.add('alerta')
+        setTimeout(function(){ // função que age com o tempo definifo
+            aviso.textContent = '' // remove o texto de aviso
+            aviso.classList.remove('alerta') // remove a classe
+        }, 2000) // após 2 segundos
+    }
+}
+
+// CALCULAR MÉDIA APÓS O CLICK NO BOTÃO
+btnCalcular.addEventListener('click', function(e) {
+    console.log('Calcular média')
+    // pegar o valor que está dentro da caixa
+    // usar o métodp parseFloat p converter string p float
+    let nota1 = parseFloat(cxNota1.value)
+    let nota2 = parseFloat(cxNota2.value)
+    let media = calcularMedia(nota1, nota2)
+
+    console.log(nota1)
+    console.log(nota2)
+    console.log(media)
+
+    if (isNaN(media) || media < 0) {
+        console.log('Não é um número')
+        cxSituacao.value = ''
+    } else {
+        cxMedia.value = parseFloat(media)
+        cxSituacao.value = situacaoFinal(media)
+        formatarSituacao(situacaoFinal(media))
+    }
+    e.preventDefault()
+})
+
+// APÓS TIRAR AS CLASS DA CX SITUAÇÃO
+btnLimpar.addEventListener('click', function() {
+    cxSituacao.classList.remove('aprovado')
+    cxSituacao.classList.remove('reprovado')
+    cxSituacao.classList.remove('recuperação')
+})
