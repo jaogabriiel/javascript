5de829c2872ff7daeb6fcfb9d6c24940d0292435
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-21 20:13:20 -0300

    ex

--- exercicoform/script.js
+++ exercicoform/script.js
@@ -4,8 +4,8 @@ let cxN3 = document.querySelector('#nota3')
 let cxN4 = document.querySelector('#nota4')
 
 let enviar = document.querySelector('#enviar')
-let limpar = document.querySelector('#enviar')
-let aviso = document.querySelector('#aviso')
+let limpar = document.querySelector('#limpar')
+let cxSituacao = document.querySelector('#aviso')
 let cxMedia = document.querySelector('#media')
 
 function calcular(n1, n2, n3, n4) {
@@ -24,41 +24,39 @@ function situacao(mediaFinal) {
     return situacao
 }
 
-function cxSituacao(situacao) {
+function definirSituacao(situacao) {
     switch(situacao) {
         case 'Aprovado':
-            aviso.textContent = "Aprovado"
-            aviso.classList.remove('reprovado')
-            aviso.classList.remove('recuperacao')
-            aviso.classList.add('aprovado')
+            cxSituacao.classList.remove('reprovado')
+            cxSituacao.classList.remove('recuperacao')
+            cxSituacao.classList.add('aprovado')
             break
 
         case 'Reprovado':
-            aviso.textContent = "Reprovado"
-            aviso.classList.remove('aprovado')
-            aviso.classList.remove('recuperacao')
-            aviso.classList.add('aprovado')
+            cxSituacao.classList.remove('aprovado')
+            cxSituacao.classList.remove('recuperacao')
+            cxSituacao.classList.add('aprovado')
             break
 
         case 'Recuperacao':
-            aviso.textContent = "Recuperação"
-            aviso.classList.remove('aprovado')
-            aviso.classList.remove('reprovado')
-            aviso.classList.add('recuperacao')
+            cxSituacao.classList.remove('aprovado')
+            cxSituacao.classList.remove('reprovado')
+            cxSituacao.classList.add('recuperacao')
             break
         
         default:
-            aviso.textContent = "Impossível definir situação"
+            cxSituacao.textContent = "Impossível definir situação"
     }
 }
 
-function validar() {
-    let nota1 = cxN1.value
-    let nota2 = cxN2.value
-    let nota3 = cxN3.value
-    let nota4 = cxN4.value
+function validar(num) {
+    let nota1 = parseFloat(cxN1.value)
+    let nota2 = parseFloat(cxN2.value)
+    let nota3 = parseFloat(cxN3.value)
+    let nota4 = parseFloat(cxN4.value)
 
     if (nota1 > 10 || nota1 < 0 || nota2 > 10 || nota2 < 0 || nota3 > 10 || nota3 < 0 || nota4 > 10 || nota4 < 0) {
+        form.reset()
         aviso.textContent = "Digite um número entre 1 e 10"
         aviso.classList.add('alerta')
         setTimeout(function() {
@@ -68,22 +66,22 @@ function validar() {
     }
 }
 
-enviar.addEventListener('click',function(e){
-    let num1 = cxN1.value
-    let num2 = cxN2.value
-    let num3 = cxN3.value
-    let num4 = cxN4.value
-    let media = calcular(num1, num2, num3,num4)
+enviar.addEventListener('click', function(e) {
+    let num1 = parseFloat(cxN1.value)
+    let num2 = parseFloat(cxN2.value)
+    let num3 = parseFloat(cxN3.value)
+    let num4 = parseFloat(cxN4.value)
+    let media = calcular(num1, num2, num3, num4)
 
-    if (isNaN(media) || media > 10) {
-        cxMedia.textContent = "Informe os núemeros corretamente"
-        aviso.textContent = ''
+    if (isNaN(media || media < 0) || media > 10) {
+        cxSituacao.value = ''
     } else {
-        cxMedia.value = parseFloat(media).toFixed(1)
-        aviso.textContent = situacao(media)
+        cxMedia.value = parseFloat(media)
+        cxSituacao.value = situacao(media)
+        definirSituacao(situacao(media))
     }
     e.preventDefault()
-}) 
+})
 
 limpar.addEventListener('click', function() {
     cxMedia.textContent = ''
